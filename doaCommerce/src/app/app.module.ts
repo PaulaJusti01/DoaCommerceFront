75c5af334b63804880cd12eb02764c240c995c2e
unknown <portocrisel@hotmail.com>
2020-10-01 16:14:35 -0300

    criação do contato

--- doaCommerce/src/app/app.module.ts
+++ doaCommerce/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
