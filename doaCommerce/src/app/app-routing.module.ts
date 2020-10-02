f586a2ce8e63a7c95e745eb85b7eb2b98e80a9af
Paula Justi da Silva <paulajusti01@hotmail.com>
2020-10-02 09:07:37 -0300

    Conflitos resolvidos e rotas criadas

--- doaCommerce/src/app/app-routing.module.ts
+++ doaCommerce/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: 'home', pathMatch:'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent},
+  { path: 'saiba-mais', component: SaibaMaisComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- doaCommerce/src/app/app.module.ts
+++ doaCommerce/src/app/app.module.ts
@@ -3,33 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
 import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
-=======
+
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
->>>>>>> home-paula
-=======
 import { ContatoComponent } from './contato/contato.component';
->>>>>>> contato-Carol
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    SaibaMaisComponent
-=======
+    SaibaMaisComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
->>>>>>> home-paula
-=======
+    FooterComponent,
     ContatoComponent
->>>>>>> contato-Carol
+
   ],
   imports: [
     BrowserModule,
